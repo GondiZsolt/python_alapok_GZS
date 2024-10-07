71fbcb2bee577d06f9e9f974f0310976c8fd9afb
Gondi Zsolt <gondi.zsolt@diak.szbi-pg.hu>
2024-10-07 10:42:58 +0200

    continue project

---
+++ 06_while_ciklus.py
@@ -0,0 +1,26 @@
+# szam = 1
+# while szam <= 10:
+#     print(szam)
+#     szam = szam + 1
+
+# folytatja = True
+# while folytatja:
+#     print('Vidd ki a szemetet!')
+#     valasz = input('Mondjam még egyszer? (i/n)')
+#     if valasz == 'n':
+#         folytatja = False
+# print('>> Program vége! <<')
+
+szam = 1
+while szam <= 10:
+    if szam % 2 == 0:
+        print(szam)
+    szam += 1
+
+"""2. Feladat
+Írj egy programot, amely csökkenő sorrendben írja ki a számokat 1 és 10 között!
+"""
+
+"""3. Feladat
+Írj egy programot, amely kiírja a páratlan számokat csökkenő sorrendben 1 és 10 között!
+"""
