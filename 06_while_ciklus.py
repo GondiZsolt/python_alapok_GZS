a00b77855ed2a982d1347ee2e5996c4988ee0810
GondiZsolt <gondi.zsolt@diak.szbi-pg.hu>
2024-10-07 18:20:51 +0200

    Add files via upload

--- 06_while_ciklus.py
+++ 06_while_ciklus.py
@@ -21,6 +21,17 @@ while szam <= 10:
 Írj egy programot, amely csökkenő sorrendben írja ki a számokat 1 és 10 között!
 """
 
+# szam = 10
+# while szam >=1:
+#     print(szam)
+#     szam -= 1
+
 """3. Feladat
 Írj egy programot, amely kiírja a páratlan számokat csökkenő sorrendben 1 és 10 között!
 """
+
+# szam = 10
+# while szam >=1:
+#     if szam % 2 == 1:
+#         print(szam)
+#     szam -= 1
