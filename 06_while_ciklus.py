c2b7ec886b19e6c0e27f5952d1f9d3a39a4d9039
Gondi Zsolt <gondi.zsolt@diak.szbi-pg.hu>
2024-10-14 09:14:04 +0200

    continue project

--- 06_while_ciklus.py
+++ 06_while_ciklus.py
@@ -11,11 +11,11 @@
 #         folytatja = False
 # print('>> Program vége! <<')
 
-szam = 1
-while szam <= 10:
-    if szam % 2 == 0:
-        print(szam)
-    szam += 1
+# szam = 1
+# while szam <= 10:
+#     if szam % 2 == 0:
+#         print(szam)
+#     szam += 1
 
 """2. Feladat
 Írj egy programot, amely csökkenő sorrendben írja ki a számokat 1 és 10 között!
@@ -35,3 +35,17 @@ while szam <= 10:
 #     if szam % 2 == 1:
 #         print(szam)
 #     szam -= 1
+
+"""5. Feladat
+Írj egy programot, amely a felhasználótól páros számot kér be. Amennyiben a megadott szám páratlan,
+újra és újra megtörténik az adatbekérés mindaddig, amíg végül páros számot nem ad meg a felhasználó.
+"""
+
+paros_szam = False
+
+while paros_szam == False:
+    felhasznaloi_input = int(input("Adj meg egy páros számot!"))
+    if felhasznaloi_input % 2 == 0:
+        paros_szam = True
+    if paros_szam == True:
+        print("kösz mán")
